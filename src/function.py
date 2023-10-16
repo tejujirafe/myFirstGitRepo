497cb18dbad2937f1a82a8ed732026e7898b9c52
tejashree-jirafe <tejaswinijirafe@gmail.com>
2023-10-17 00:29:11 +0530

    implements logic to find square of any number in python

---
+++ src/function.py
@@ -0,0 +1,64 @@
+
+#! Q.1 write a program to find the square using parameterized function
+def sqr(s):
+    s=int(input("please enter the number to find the square "))
+    a=s*s
+    print("square is: ",a)
+sqr(3)
+
+#! Q.2 write a program to find cube using function
+# def cub(c):
+#         c=int(input("please enter the number to find the cube"))
+#         b=c*c*c
+#         print("cube of the number is: ",b)
+# cub(2)
+
+#! Q.3 Write a python program find addition, subtraction, multiplication and division using the  function
+# def calc():
+#     print("please enter number to perform the addition: ")
+#     a=int(input("first number:"))
+#     b=int(input("second number"))
+#     add=a+b
+#     print("addition is: ", add)
+
+#     print("please enter number to perform the substraction: ")
+#     a=int(input("first number:"))
+#     b=int(input("second number"))
+#     sub=b-a
+#     print("substraction is: ", sub)
+
+#     print("please enter number to perform the multiplication: ")
+#     a=int(input("first number:"))
+#     b=int(input("second number"))
+#     mul=a*b
+#     print("multiplication is: ", mul)
+
+#     print("please enter number to perform the division: ")
+#     a=int(input("first number:"))
+#     b=int(input("second number"))
+#     div=a/b
+#     print("division is: ", div)
+# calc()
+
+#! Q.4 write a python  program find area of circle and area of triangle using function
+# def area():
+#     r=int(input("please enter the radius of the circle to find its area: "))
+#     aoc=3.14*r*r
+#     print("area of the circle is: ",aoc)
+
+#     b=int(input("please enter the base of the triangle to find its area"))
+#     h=int(input("please enter the height of the triangle"))
+#     aot=(1/2)*b*h
+#     print("area of the triangle is: ",aot)
+# area()
+
+#! Q.5 Write a python program.. to find simple rate of intreste using function
+# def si():
+#     p=int(input("please enter the principle amount: "))
+#     r=int(input("please input the rate of interest: "))
+#     t=int(input("please enter the time period: "))
+
+#     si=(p*r*t)/100
+
+#     print("simple interest is calculated to be",si)
+# si()
